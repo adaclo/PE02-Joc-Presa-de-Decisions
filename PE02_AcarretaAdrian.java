b246c874370f6e3ac28576cefca5b413f6d3726b
Adrian Acarreta <alu.adrian.acarreta@mataro.epiaedu.cat>
2025-10-02 16:47:08 +0200

    Declaració de variables

---
+++ PE02_AcarretaAdrian.java
@@ -0,0 +1,22 @@
+package Activitats.PE02;
+
+import java.util.Scanner;
+
+public class PE02_AcarretaAdrian {
+    public static void main(String[] args) {
+        // DECLARACIO DE VARIABLES
+        boolean alarmClock = false;
+        boolean pillow = false;
+        boolean knife = false;
+        boolean medkit = false;
+        boolean shotgun = false;
+        boolean radio = false;
+        boolean paranoia = false;
+        boolean hungry = false;
+        boolean wounds = false;
+        Scanner e = new Scanner(System.in);
+
+        System.out.println("");
+        System.out.println("Ha començat l'alarma nuclear a tot el país, però per sort l'any passat vas instal·lar un búnquer nuclear al teu soterrani, tens poc temps, actua amb cap.");
+    }
+}
